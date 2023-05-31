44f16a5d23385fa85143a78094fc37fecbbf5099
Joel Andrew Bianchi <JABianchi@gmail.com>
2023-05-31 09:55:49 -0400

    Added Parambir's Arduino Husky starter code

---
+++ HuskyCode/huskylens_i2c_tag_motion_tracker/huskylens_i2c_tag_motion_tracker.ino
@@ -0,0 +1,62 @@
+/***************************************************
+ HUSKYLENS An Easy-to-use AI Machine Vision Sensor
+ <https://www.dfrobot.com/product-1922.html>
+ 
+ Connection and Diagram can be found here
+ <https://wiki.dfrobot.com/HUSKYLENS_V1.0_SKU_SEN0305_SEN0336#target_23>
+ 
+ Make sure to install library:
+ A. Download code from: https://github.com/HuskyLens/HUSKYLENSArduino/archive/refs/heads/master.zip
+ B. Unzip main file
+ C. Sketch>Include Libraries>Add .zip library
+ D. Choose the HuskyLens.zip file
+ 
+ Older Version of Arduino Code online:
+ https://create.arduino.cc/editor/parambir17/b9a71742-bf70-4b8a-ac04-dd9e54f7f769/preview
+ 
+ ****************************************************/
+
+#include "HUSKYLENS.h"
+
+HUSKYLENS huskylens;
+//HUSKYLENS green line >> SDA; blue line >> SCL
+void printResult(HUSKYLENSResult result);
+
+void setup() {
+    Serial.begin(9600);
+    Wire.begin();
+    while (!huskylens.begin(Wire))
+    {
+        Serial.println(F("Begin failed!"));
+        Serial.println(F("1.Please recheck the \"Protocol Type\" in HUSKYLENS (General Settings>>Protocol Type>>I2C)"));
+        Serial.println(F("2.Please recheck the connection."));
+        delay(100);
+    }
+}
+
+void loop() {
+    if (!huskylens.request()) Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection!"));
+    else if(!huskylens.isLearned()) Serial.println(F("Nothing learned, press learn button on HUSKYLENS to learn one!"));
+    else if(!huskylens.available()) Serial.println(F("No block or arrow appears on the screen!"));
+    else
+    {
+        Serial.println(F("###########"));
+        while (huskylens.available())
+        {
+            HUSKYLENSResult result = huskylens.read();
+            printResult(result);
+        }    
+    }
+}
+
+void printResult(HUSKYLENSResult result){
+    if (result.command == COMMAND_RETURN_BLOCK){
+        Serial.println(String()+F("Block:xCenter=")+result.xCenter+F(",yCenter=")+result.yCenter+F(",width=")+result.width+F(",height=")+result.height+F(",ID=")+result.ID);
+    }
+    else if (result.command == COMMAND_RETURN_ARROW){
+        Serial.println(String()+F("Arrow:xOrigin=")+result.xOrigin+F(",yOrigin=")+result.yOrigin+F(",xTarget=")+result.xTarget+F(",yTarget=")+result.yTarget+F(",ID=")+result.ID);
+    }
+    else{
+        Serial.println("Object unknown!");
+    }
+}
